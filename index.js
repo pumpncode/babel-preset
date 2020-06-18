d996a51dc9100ce9186043b2fbacff54fe0eac78
nnmrts <nanomiratus@gmail.com>
2020-06-18 17:51:02 +0200

    feat: :sparkles: add and remove plugins
    bye bye flow, hello es proposals!

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 module.exports = () => ({
-    "presets": [
-        "@babel/preset-env",
-        "@babel/preset-flow"
-    ],
-    "plugins": [
-        "@babel/plugin-transform-flow-strip-types",
-        "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-proposal-optional-chaining",
-        [
-            "@babel/plugin-transform-runtime",
-            {
-                "regenerator": true
-            }
-        ]
-    ]
+    "comments": true,
+	"plugins": [
+		[
+			"@babel/plugin-proposal-class-properties",
+			{
+				"loose": true
+			}
+		],
+		[
+			"@babel/plugin-proposal-private-methods",
+			{
+				"loose": true
+			}
+		],
+		"@babel/plugin-proposal-export-default-from",
+		"@babel/plugin-proposal-logical-assignment-operators",
+		"@babel/plugin-proposal-partial-application",
+		[
+			"@babel/plugin-proposal-pipeline-operator",
+			{
+				"proposal": "minimal"
+			}
+		],
+		"@babel/plugin-proposal-throw-expressions",
+		[
+			"@babel/plugin-proposal-private-property-in-object",
+			{
+				"loose": true
+			}
+		],
+		[
+			"@babel/plugin-syntax-module-attributes",
+			{
+				"version": "may-2020"
+			}
+		]
+	],
+	"presets": [
+		[
+			"@babel/preset-env",
+			{
+				"targets": {
+					"node": "14"
+				}
+			}
+		]
+	]
 });
